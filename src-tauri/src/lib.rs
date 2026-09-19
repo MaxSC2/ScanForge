d97db3a14b6311b728ddc75295d33bc50e86f235
MaxSC2 <155305256+MaxSC2@users.noreply.github.com>
2026-09-20 02:36:28 +0500

    fix: import OCR cancellation command

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -19,7 +19,7 @@ use domain_storage::{
 };
 use imagick::{check_imagick, convert_with_imagick};
 use ocr::detect::auto_detect_regions;
-use ocr::run_page_ocr;
+use ocr::{cancel_page_ocr, run_page_ocr};
 use std::io;
 use storage::{
     delete_page_image, delete_project_assets, list_project_summaries, load_latest_project_snapshot,
